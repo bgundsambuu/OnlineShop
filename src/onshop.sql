42a35559ad788c7056b21d753e33be1b815a9298
Mingwei <10962421@qq.com>
2018-10-15 11:53:19 -0500

    initialized data

---
+++ src/onshop.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Address` VALUES ('adId1', 'city14123', 'country14123', '123state14123', 'street1123123', '1', '110004123'), ('adId2', 'city14', 'country14', 'state14', 'street14', '2', '110004'), ('adId3', 'city3', 'c3', 'state3', 'street3', '3', '52556');
+INSERT INTO `Administrator` VALUES ('adminid', 'firstN1', 'lastNa1', 'phoneN1', '1');
+INSERT INTO `Customer` VALUES ('customeriD1', 'firstName', 'lastName', 'order111', '319-222-5555', '3');
+INSERT INTO `User` VALUES ('1', '1', '123', '0', 'username23'), ('2', '1', 'password2', '1', 'username2'), ('3', '1', 'password3', '2', 'username3');
+INSERT INTO `Vendor` VALUES ('1', 'firstN', 'lstN', 'name', '319-222-1111', 'status', '2');
