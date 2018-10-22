41e00d9beffc433925a4afff811a4ff7ba8988ee
amcfire <amendez.contac@gmail.com>
2018-10-22 15:08:25 -0500

    Add some valid users with valid ROLES to the SQL script

--- src/onshop.sql
+++ src/onshop.sql
@@ -1,4 +1,5 @@
-INSERT INTO `User` VALUES (1, '1', '123', '0', 'username23'), (2, '1', 'password2', '1', 'username2'), (3, '1', 'password3', '2', 'username3');
+use onshop;
+INSERT INTO `User` VALUES (1, '1', '123', 'ROLE_USER', 'andres.mendez.cor@gmail.com'), (2, '1', '123', 'ROLE_ADMIN', 'amendez.contac@gmail.com'), (3, '1', '123', 'ROLE_VENDOR', 'acorchuelo@mum.edu');
 INSERT INTO `Address` VALUES (1, 'city14123', 'country14123', '123state14123', 'street1123123', '110004123'), (2, 'city14', 'country14', 'state14', 'street14', '110004'), (3, 'city3', 'c3', 'state3', 'street3', '52556');
 INSERT INTO `Administrator` VALUES (1, 'firstN1', 'lastNa1', '130-3333-2222', 0);
 INSERT INTO `Customer` VALUES (1, 'firstName', 'lastName', '319-222-5555', 3, 3);
