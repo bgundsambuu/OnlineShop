051540e0af830421d94e297af0564effc168e2a0
bgundsambuu <33839184+bgundsambuu@users.noreply.github.com>
2018-10-25 13:43:43 -0500

    asdasd
    asdasd

--- src/main/java/com/onlineShop/service/impl/BankAPIServiceImpl.java
+++ src/main/java/com/onlineShop/service/impl/BankAPIServiceImpl.java
@@ -60,11 +60,13 @@ public class BankAPIServiceImpl implements BankAPIService {
         }
         return "404";
     }
-
+    private static String padLeftFormat(String input, int padUpTo){
+        return String.format("%0" + padUpTo + "d", Integer.parseInt(input));
+    }
     @Override
     public int callBankAPI(String cardNo, String expiredMonth, String expiredYear, String cvv, String ownerName, String zipCode, String purchaseAmnt) {
         String url = "http://localhost:5555/api/v1/payment/card-no/"+cardNo+
-                "/expired-mm/"+expiredMonth+"/expired-yyyy/"+expiredYear+"/cvv/"+cvv+"/owner-name/"+ownerName+"/zip-code/"+zipCode+"/purchase-amt/"+purchaseAmnt;
+                "/expired-mm/"+padLeftFormat(expiredMonth, 2)+"/expired-yyyy/"+expiredYear+"/cvv/"+cvv+"/owner-name/"+ownerName+"/zip-code/"+zipCode+"/purchase-amt/"+purchaseAmnt;
         try {
             System.out.println(url);
             String s = sendPOST(url);
